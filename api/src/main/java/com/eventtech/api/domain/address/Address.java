218948a02c619e5cda7a8a9ae25e76871e2147ee
M-Zweibrucker <matheuszweibrucker@gmail.com>
2024-08-14 23:08:27 -0300

    git init

---
+++ api/src/main/java/com/eventtech/api/domain/address/Address.java
@@ -0,0 +1,29 @@
+package com.eventtech.api.domain.address;
+
+import com.eventtech.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @GeneratedValue
+    @Id
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
