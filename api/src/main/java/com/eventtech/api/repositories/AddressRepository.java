3bed4bb70a09bc38d9e43c7d9b1ebb2838de2895
M-Zweibrucker <matheuszweibrucker@gmail.com>
2024-08-15 15:15:36 -0300

    feat: add repositories

---
+++ api/src/main/java/com/eventtech/api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.eventtech.api.repositories;
+
+import com.eventtech.api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository <Address, UUID> {
+}
